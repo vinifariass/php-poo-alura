8bc4a3f57999b21c1291217dd29bb9439b893c61
Vinicius <fsvinicius9@gmail.com>
2022-04-04 19:39:39 -0300

    requisicao HTTP com arquivos

---
+++ Streams/escritor-cursos.php
@@ -0,0 +1,6 @@
+<?php
+
+
+$curso = "Design Patterns PHP II: Boas praticas de programação";
+file_put_contents('cursos-php.txt', $curso, FILE_APPEND);
+//https://swapi.co/api/films/4/ acessa arquivos atravez de http e ssh
--- Streams/leitor-cursos.php
+++ Streams/leitor-cursos.php
@@ -1,4 +1,4 @@
 <?php
 
-$cursos = file_get_contents('lista-cursos.txt');
-echo $cursos;
+$cursos = file('lista-cursos.txt');
+var_dump($cursos);
