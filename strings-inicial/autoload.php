c977e69528b721674396f2ffaf7df91478a85540
ViniciusFarias <fsvinicius9@gmail.com>
2022-04-04 12:33:23 -0300

    Autoload implementado

--- strings-inicial/autoload.php
+++ strings-inicial/autoload.php
@@ -1,5 +1,11 @@
 <?php
 
-spl_autoload_register(function($classe){
-  var_dump($classe);
-})
+spl_autoload_register(function ($classe) {
+  $prefixo = "App\\";
+
+  $diretorio = __DIR__ . '/src/';
+
+  if (strncmp($prefixo, $classe, strlen($prefixo)) !== 0) {
+    return;
+  };
+});
