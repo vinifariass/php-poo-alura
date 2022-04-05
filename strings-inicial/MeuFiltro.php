0da3c75d150ba3e996c23214de0c29b8b0bb0977
ViniciusFarias <fsvinicius9@gmail.com>
2022-04-05 14:06:58 -0300

    consertando o filtro

--- strings-inicial/MeuFiltro.php
+++ strings-inicial/MeuFiltro.php
@@ -6,8 +6,8 @@ class MeuFiltro extends php_user_filter
   public $stream;
   public function onCreate()
   {
-    $this->stream = fopen('php://temp','w+'); // quando o filtro é criado
-  return $this->stream !== false;
+    $this->stream = fopen('php://temp', 'w+'); // quando o filtro é criado
+    return $this->stream !== false;
   }
 
   public function onClose()
@@ -18,18 +18,18 @@ class MeuFiltro extends php_user_filter
   public function filter($in, $out, &$consumed, $closing)
   {
     $saida = '';
-    while($bucket = stream_bucket_make_writeable($in)){
-      $linhas = explode("\n",$bucket->data);
+    while ($bucket = stream_bucket_make_writeable($in)) {
+      $linhas = explode("\n", $bucket->data);
 
-      foreach($linhas as $linha){
-        if(stripos()){
-          if(stripos($linha,'parte') !== false){
-            $saida .= "$linha\n";
+      foreach ($linhas as $linha) {
+        if (stripos($linha, 'parte') !== false) {
+          $saida .= "$linha\n";
         }
       }
     }
-    $bucketSaida = stream_bucket_new($this->stream,$aida);
-    stream_bucket_append($out,$bucketSaida);
+
+    $bucketSaida = stream_bucket_new($this->stream, $saida);
+    stream_bucket_append($out, $bucketSaida);
 
     return PSFS_PASS_ON;
   }
